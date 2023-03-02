0341547ecdb57a02fca470269ee2910375f71ebc
Vianka <vi.vargasmora@gmail.com>
2023-03-02 15:25:41 -0600

    Se crea una sola funcion para validar todos los eventos

--- js/app.js
+++ js/app.js
@@ -7,10 +7,24 @@ document.addEventListener('DOMContentLoaded', function() {
     //Asignar elementos
     //blur se ejecuta cuando se abandona el input
     //input se dispara cada vez que estemos escribiendo //validación en tiempo real
+    /*
     inputEmail.addEventListener('blur', function(e) {
         //console.log('Salí del input'); //confirma por consola que el blur funciona
         //console.log(e); //muestra información del evento 'blur'
         //console.log(e.target); //envía la información html del input hacia la consola
         console.log(e.target.value); //imprime en consola lo que se escribe en el input
     });
+    */
+    //inputEmail.addEventListener('blur', validar()); //con paréntesis manda a llamar de una vez la función
+    inputEmail.addEventListener('blur', validar); //Sin paréntesis funciona hasta que suceda el evento
+    inputAsunto.addEventListener('blur', validar);
+    inputMensaje.addEventListener('blur', validar);
+    /*
+    function validar () {
+        console.log('desde la función de validar');
+    }
+    */
+    function validar (e) {
+        console.log(e.target.value);
+    }
 });
