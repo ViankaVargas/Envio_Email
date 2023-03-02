c38d7553954a1f9b0e5a80c5c33e2531fdfb7c56
Vianka <vi.vargasmora@gmail.com>
2023-03-02 15:05:16 -0600

    Se agrega el evento blur con .addEventListener para validar el formulario

--- js/app.js
+++ js/app.js
@@ -4,5 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
 
-    console.log(inputMensaje);
+    //Asignar elementos
+    //blur se ejecuta cuando se abandona el input
+    //input se dispara cada vez que estemos escribiendo //validación en tiempo real
+    inputEmail.addEventListener('blur', function(e) {
+        //console.log('Salí del input'); //confirma por consola que el blur funciona
+        //console.log(e); //muestra información del evento 'blur'
+        //console.log(e.target); //envía la información html del input hacia la consola
+        console.log(e.target.value); //imprime en consola lo que se escribe en el input
+    });
 });
